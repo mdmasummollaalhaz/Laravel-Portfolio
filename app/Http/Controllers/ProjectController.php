dcac41749fd7b56305d1ce8655fdaf3c1a54aed4
Masum Mollo <freelancermasummolla@gmail.com>
2023-07-21 00:43:41 +0600

    Project data

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    // Page
+    function page(Request $request){
+        // return view('pages.projects');
+    }
+
+    // Projects Data
+    function projectsData(Request $request){
+        return DB::table('projects')->get();
+    }
 }
