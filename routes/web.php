d5e09957ba0db1af3b4006e0759b78373c3a9e98
Masum Mollo <freelancermasummolla@gmail.com>
2023-07-21 00:28:42 +0600

    Pages Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Pages Routes
+Route::get('/', [HomeController::class, 'page']);
+Route::get('/contact', [ContactController::class, 'page']);
+Route::get('/projects', [ProjectController::class, 'page']);
+Route::get('/resume', [ResumeController::class, 'page']);
