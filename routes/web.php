ab92bc4a3dab6d65b90093ad208892c8022a5223
Masum Mollo <freelancermasummolla@gmail.com>
2023-07-21 01:21:24 +0600

    Ajax Call Routes

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,17 @@ Route::get('/', [HomeController::class, 'page']);
 Route::get('/contact', [ContactController::class, 'page']);
 Route::get('/projects', [ProjectController::class, 'page']);
 Route::get('/resume', [ResumeController::class, 'page']);
+
+
+
+// Ajax Call Routes
+Route::get('/heroData', [HomeController::class, 'heroData']);
+Route::get('/aboutData', [HomeController::class, 'aboutData']);
+Route::get('/socialData', [HomeController::class, 'socialData']);
+Route::get('/projectsData', [HomeController::class, 'projectsData']);
+Route::get('/resumeLink', [HomeController::class, 'resumeLink']);
+Route::get('/experiencesData', [HomeController::class, 'experiencesData']);
+Route::get('/educationData', [HomeController::class, 'educationData']);
+Route::get('/skillsData', [HomeController::class, 'skillsData']);
+Route::get('/languagesData', [HomeController::class, 'languagesData']);
+Route::get('/contactRequest', [HomeController::class, 'contactRequest']);
