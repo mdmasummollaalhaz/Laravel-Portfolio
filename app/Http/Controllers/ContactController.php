be79854fe81d6c68186046b10f93fdd01e78ab4a
Masum Mollo <freelancermasummolla@gmail.com>
2023-07-21 01:20:49 +0600

    Contact Request

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ContactController extends Controller
 {
-    //
+    // Page
+    function page(Request $request){
+        // return view('pages.contact');
+    }
+
+    // Contact Request
+    function contactRequest(Request $request){
+        return DB::table('contacts')->first();
+    }
 }
