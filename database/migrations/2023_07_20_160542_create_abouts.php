ba1da24348c797fe00df0a97c760c5a0bbb3f8ac
Masum Mollo <freelancermasummolla@gmail.com>
2023-07-20 22:37:19 +0600

    abouts tables

--- database/migrations/2023_07_20_160542_create_abouts.php
+++ database/migrations/2023_07_20_160542_create_abouts.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('abouts', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title', 100);
+            $table->text('details');
+
+            // Common table
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
