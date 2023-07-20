b6396cffac9851062688b42c210b0cda63a0eb03
Masum Mollo <freelancermasummolla@gmail.com>
2023-07-21 00:37:52 +0600

    Home data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    //
+    // Page
+    function page(Request $request){
+        // return view('pages.home');
+    }
+
+    // Hero Data
+    function heroData(Request $request){
+        return DB::table('heroproperties')->first();
+    }
+
+    // About Data
+    function aboutData(Request $request){
+        return DB::table('abouts')->first();
+    }
+
+    //Socials Data
+    function socialData(Request $request){
+        return DB::table('socials')->first();
+    }
 }
