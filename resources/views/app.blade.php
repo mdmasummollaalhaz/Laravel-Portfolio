e7be3b5dd024097d1ec7c6a825870edbe7f39afc
Masum Mollo <freelancermasummolla@gmail.com>
2023-07-21 12:24:16 +0600

    Asset and SEO added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<html lang="en" data-bs-theme="light">
 
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    {{-- SEO --}}
+    <title>Portfolio</title>
     <meta name="description" content="" />
     <meta name="author" content="" />
 
-    <title>Portfolio</title>
+    <meta name="og:site_name" content="" />
+    <meta name="og:url" content="" />
+    <meta name="og:title" content="" />
+    <meta name="og:image" />
+    <meta name="og:description" content="" />
+
+
+    {{-- Asset files --}}
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
+    <link href="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
+    <script src="{{ asset('js/axios.min.js') }}"></script>
 </head>
-<body>
+
+<body class="d-flex flex-column h-100">
 
     @include('components.navbar')
 
