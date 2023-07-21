5280fdc328e328290b6530bf96d003da6bb50b67
Masum Mollo <freelancermasummolla@gmail.com>
2023-07-21 17:55:24 +0600

    API added

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Ajax Call Routes
+Route::get('/heroData', [HomeController::class, 'heroData']);
+Route::get('/aboutData', [HomeController::class, 'aboutData']);
+Route::get('/socialData', [HomeController::class, 'socialData']);
+Route::get('/projectsData', [ProjectController::class, 'projectsData']);
+Route::get('/resumeLink', [ResumeController::class, 'resumeLink']);
+Route::get('/experiencesData', [ResumeController::class, 'experiencesData']);
+Route::get('/educationsData', [ResumeController::class, 'educationsData']);
+Route::get('/skillsData', [ResumeController::class, 'skillsData']);
+Route::get('/languagesData', [ResumeController::class, 'languagesData']);
+Route::get('/contactRequest', [ContactController::class, 'contactRequest']);
