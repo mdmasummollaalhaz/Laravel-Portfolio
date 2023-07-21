80114f88d4bfb913cf502e0de65380e9c15cfa28
Masum Mollo <freelancermasummolla@gmail.com>
2023-07-22 01:42:52 +0600

    axios added

--- resources/views/components/project-list.blade.php
+++ resources/views/components/project-list.blade.php
@@ -6,8 +6,6 @@
         </div>
         <div class="row gx-5 justify-content-center">
             <div id="project-list" class="col-lg-11 col-xl-9 col-xxl-8">
-
-
             </div>
         </div>
     </div>
@@ -21,12 +19,21 @@
 
         try {
 
-            let res=await axios.get(URL);
+            // Loader Show - Content Hide
+            document.getElementById('loading-div').classList.remove('d-none');
+            document.getElementById('content-div').classList.add('d-none');
+
+            let response=await axios.get(URL);
+
+            // Loader Hide - Content Show
+            document.getElementById('loading-div').classList.add('d-none');
+            document.getElementById('content-div').classList.remove('d-none');
+
             // if(res.data.length===0){
             //     alert(`No Found Data`);
             // }
 
-            res.data.forEach((item)=>{
+            response.data.forEach((item)=>{
                 document.getElementById("project-list").innerHTML+= (
                     `<div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
                         <div class="card-body p-0">
@@ -34,6 +41,7 @@
                                 <div class="p-5">
                                     <h2 class="fw-bolder">${item['title']}</h2>
                                     <p>${item['details']}</p>
+                                    <a class="text-decoration-none" target="_blank" href="${item['previewLink']}">View Project</a>
                                 </div>
                                 <img class="img-fluid" src="${item['thumbnailLink']}}" alt="..." />
                             </div>
@@ -42,8 +50,8 @@
                 )
             })
         }
-        catch (e){
-            console.log(e);
+        catch (error){
+            alert(error);
         }
     }
 
