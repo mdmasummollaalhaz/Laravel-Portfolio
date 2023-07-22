5f767418560412459ce943c31512b9e3e6b710d8
Masum Mollo <freelancermasummolla@gmail.com>
2023-07-22 10:43:44 +0600

    axios added

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -5,22 +5,40 @@
             <div class="col-xxl-5">
                 <!-- Header text content-->
                 <div class="text-center text-xxl-start">
-                    <div class="badge bg-gradient-primary-to-secondary text-white mb-4"><div class="text-uppercase">Design &middot; Development &middot; Marketing</div></div>
-                    <div class="fs-3 fw-light text-muted">I can help your business to</div>
-                    <h1 class="display-3 fw-bolder mb-5"><span class="text-gradient d-inline">Get online and grow fast</span></h1>
+                    <div class="badge bg-gradient-primary-to-secondary text-white mb-4"><div id="keyLine" class="text-uppercase"></div></div>
+                    <div class="fs-3 fw-light text-muted" id="short_title"></div>
+                    <h1 class="display-3 fw-bolder mb-5"><span class="text-gradient d-inline" id="title"></span></h1>
                     <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
-                        <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="resume.html">Resume</a>
-                        <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="projects.html">Projects</a>
+                        <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="{{ url('/resume') }}">Resume</a>
+                        <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="{{ url('/projects') }}">Projects</a>
                     </div>
                 </div>
             </div>
             <div class="col-xxl-7">
                 <div class="d-flex justify-content-center mt-5 mt-xxl-0">
                     <div class="profile">
-                        <img class="profile-img" src="assets/profile.png" alt="..." />
+                        <img class="profile-img" id="profileImg" src="" alt="..." />
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </header>
+
+<script>
+
+    getHero();
+    async function getHero(){
+        try{
+            let URL="/heroData";
+            let response = await axios.get(URL);
+            document.getElementById('keyLine').innerHTML=response.data['keyLine'];
+            document.getElementById('short_title').innerHTML=response.data['short_title'];
+            document.getElementById('title').innerHTML=response.data['title'];
+            document.getElementById('profileImg').innerHTML=response.data['img'];
+        }
+        catch(error){
+            alert(error);
+        }
+    }
+</script>
